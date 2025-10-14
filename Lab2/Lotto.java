46fe486546033f5540ab496bc67fabdf1af3dcc4
michal firek <mfirek@student.agh.edu.pl>
2025-10-14 15:33:24 +0200

    lotto

--- Lab2/Lotto.java
+++ Lab2/Lotto.java
@@ -1,2 +1,23 @@
-public class Lotto {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Lotto
+{
+    public static void main(String[] args)
+    {
+        ArrayList<Integer> liczby = new ArrayList<>();
+        Random losowy = new Random();
+
+        while (liczby.size() < 6)
+        {
+            int liczba = losowy.nextInt(49) + 1;
+            if (!liczby.contains(liczba))
+            {
+                liczby.add(liczba);
+            }
+        }
+
+        System.out.println(liczby);
+    }
 }
+
